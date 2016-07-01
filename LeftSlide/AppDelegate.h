bb3bc212e9e00d14e69e022776e5042acb57979c
范煜 <431917467@qq.com>
2016-07-01 10:16:30 +0800

    Initial Commit

---
+++ LeftSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeftSlide
+//
+//  Created by hiteam on 16/7/1.
+//  Copyright © 2016年 steam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeftSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeftSlide
+//
+//  Created by hiteam on 16/7/1.
+//  Copyright © 2016年 steam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
